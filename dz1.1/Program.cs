540d963c9ece60bc6a6950d84ae0b4345d81bdf1
ole-gg <oleg.fedorov2002@mail.ru>
2024-10-13 19:26:36 +0300

    Add files via upload

---
+++ dz1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i < num+1; i++)
+{
+    if (Prostoe_li(i))
+    {
+        Console.WriteLine(i);
+    }
+}
+    
+
+bool Prostoe_li(int num)
+{
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
