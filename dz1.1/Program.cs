ecc19f643ae7fac4795b3c8afde5977a13df0d44
ole-gg <oleg.fedorov2002@mail.ru>
2024-10-15 20:58:08 +0300

    Update Program.cs
    фанкшн хэс бин ренэймд

--- dz1.1/Program.cs
+++ dz1.1/Program.cs
@@ -4,14 +4,14 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 2; i < num+1; i++)
 {
-    if (Prostoe_li(i))
+    if (IsPrime(i))
     {
         Console.WriteLine(i);
     }
 }
     
 
-bool Prostoe_li(int num)
+bool IsPrime(int num)
 {
     for (int i = 2; i < num; i++)
     {
